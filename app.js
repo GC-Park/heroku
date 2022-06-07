c785d9cc76199c8bd8ce2391d0815a8d3e4e512c
GC-Park <kuyt3122@naver.com>
2022-06-07 11:27:29 +0900

    make it better

--- app.js
+++ app.js
@@ -57,9 +57,9 @@ app.get('/auth/logout', logoutController)
 
 let port = process.env.PORT;
 if(port==null || port==""){
-    port = 4000;
+    port = 5000;
 }
 
 app.listen(port, ()=>{
-    console.log('App listening on port 4000')
+    console.log('App listening on port 5000')
 })
