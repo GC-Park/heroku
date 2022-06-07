42dafd632370f6b08b204647ce0c1145371f3c53
GC-Park <kuyt3122@naver.com>
2022-06-07 11:11:30 +0900

    make it better

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ app.get('/auth/logout', logoutController)
 // })
 
 
-
-app.listen(4000, ()=>{
+const PORT = process.env.PORT
+app.listen(PORT, ()=>{
     console.log('App listening on port 4000')
 })
