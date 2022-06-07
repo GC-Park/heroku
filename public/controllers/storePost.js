e804fc4e03760bfb7ee09f8d17dee5c5758ddfd6
GC-Park <kuyt3122@naver.com>
2022-06-07 11:01:56 +0900

    make it better

--- public/controllers/storePost.js
+++ public/controllers/storePost.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 module.exports = async (req, res) => {
     let image = req.files.image;
-    image.mv(path.resolve(__dirname, '..','public/img', image.name), async(error)=>{
+    image.mv(path.resolve(__dirname, '..','public/assets/img', image.name), async(error)=>{
         await BlogPost.create({
             ...req.body,
             image: '../public/assets/img/' + image.name
