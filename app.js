172d8197d8939138764c8ad2cf16baf8aa627c5b
GC-Park <kuyt3122@naver.com>
2022-06-07 11:14:26 +0900

    make it better

--- app.js
+++ app.js
@@ -55,7 +55,11 @@ app.get('/posts/new', authMiddleware, newPostController)
 app.get('/auth/logout', logoutController)
 
 
-const PORT = process.env.PORT
-app.listen(PORT, ()=>{
+let port = process.env.PORT;
+if(port==null || port==""){
+    port = 4000;
+}
+
+app.listen(port, ()=>{
     console.log('App listening on port 4000')
 })
